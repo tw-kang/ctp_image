d8a4a223b58b705ef0348ad92d74e0baaec07d5f
tw-kang <twkang@cubrid.com>
2024-07-30 03:44:45 +0900

    separate credential from image

--- shell/controller/docker-entrypoint.sh
+++ shell/controller/docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Start SSH service
 sudo /usr/sbin/sshd
 ulimit -c 1024
 
+# Source the bash profile to set environment variables
 source ~/.bash_profile
 
+# Execute the passed command
 exec "$@"
--- shell/worker/docker-entrypoint.sh
+++ shell/worker/docker-entrypoint.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Start SSH service
 sudo /usr/sbin/sshd
 ulimit -c 1024
 
+# Source the bash profile to set environment variables
 source ~/.bash_profile
 
+# Define the run_checkout function
+run_checkout() {
+    git clone --depth 1 -b develop https://${GITHUB_TOKEN}@github.com/CUBRID/cubrid-testcases.git && \
+    git clone --depth 1 -b develop https://${GITHUB_TOKEN}@github.com/CUBRID/cubrid-testcases-private-ex.git
+}
+
+# Call the run_checkout function by default
+run_checkout
+
+# Execute the passed command
 exec "$@"
